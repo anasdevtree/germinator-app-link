e44c2b821ae04188953b942550dae0c6a29afcf0
Anas Iqbal (UAE) <AIqbal@nuqulgroup.com>
2019-08-18 11:12:33 +0400

    Updates

---
+++ app/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Fine Germinator Augmented Reality Game, Play Now. </title>
+<meta charset="utf-8" />
+<?php include 'header.php';?>
+</head>
+<body onload="DetectAndServe()">
+
+<?php include 'footer.php';?>
+</body>
+</html>
