dea3e42e64c3777c4d10c7f985a0d82804014d0d
Anas Iqbal (UAE) <AIqbal@nuqulgroup.com>
2019-08-18 10:51:40 +0400

    Updates

--- app/header.php
+++ app/header.php
@@ -1,4 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
+
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145733623-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
